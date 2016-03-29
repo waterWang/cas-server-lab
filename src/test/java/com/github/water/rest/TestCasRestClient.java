7eb050eba50d865bb76118be40224de15aa4b492
wangweiwei <weiwei.wang@dinglicom.com>
2016-03-29 12:29:29 +0800

    test

--- src/test/java/com/github/water/rest/TestCasRestClient.java
+++ src/test/java/com/github/water/rest/TestCasRestClient.java
@@ -22,17 +22,6 @@ public final class TestCasRestClient {
 	private TestCasRestClient() {
 	}
 
-	/*
-	 * public static String getTicket(final String server, final String
-	 * username, final String password, final String service) { notNull(server,
-	 * "server must not be null"); notNull(username,
-	 * "username must not be null"); notNull(password,
-	 * "password must not be null"); notNull(service,
-	 * "service must not be null"); String ticketGrantingTicket =
-	 * getTicketGrantingTicket(server, username, password);
-	 * LOG.info("TGT="+ticketGrantingTicket); return
-	 * getServiceTicket(server,ticketGrantingTicket , service); }
-	 */
 	private static String getServiceTicket(final String server,
 			final String ticketGrantingTicket, final String service) {
 		if (ticketGrantingTicket == null)
@@ -166,12 +155,13 @@ public final class TestCasRestClient {
 	}
 
 	public static void main(final String[] args) {
-		// cas server的地址，http或者https都可以
-		final String server = "https://wangweiwei:8443/cas/v1/tickets";
+		// cas server的登录API地址，http或者https都可以
+		final String server = "http://139.214.220.47:8442/cas/v1/tickets";
 		// 现在使用邮箱、手机登录
-		 String username = "user@localhost";
-		 String password = "user";
-		 String service = "http://172.16.40.40:8080/j_spring_cas_security_check";
+		 String username = "baijie";
+		 String password = "000000";
+		 //客户端ip，端口
+		 String service = "http://ip:port/j_spring_cas_security_check";
 
 		String ticketGrantingTicket = getTicketGrantingTicket(server, username,
 				password);
@@ -180,6 +170,7 @@ public final class TestCasRestClient {
 
 		System.out.println("TGT-------" + ticketGrantingTicket);
 		System.out.println("ST-------" + serviceTicket);
+		System.out.println("url----" + service +"?ticket=" +serviceTicket);
 		// 浏览器上输入 service?ticket=上面获取的ST 即可登录
 		
 //		String logoutResult = logout(server , "TGT-2-TntP4pn1rQAowRKVlW7DuofZbQhxNOxsGhMjBRtmCxL7xhaRgD-cas01.example.org");
